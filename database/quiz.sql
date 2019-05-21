4c36a03848cac110e0240ac9997bc6aa216ccc7a
Dominic <pocaand@gmail.com>
2019-05-21 18:49:23 +0800

    Final

--- database/quiz.sql
+++ database/quiz.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `quiz` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `quiz`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: quiz
@@ -80,4 +82,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-05-18  6:19:58
+-- Dump completed on 2019-05-18  9:48:04
